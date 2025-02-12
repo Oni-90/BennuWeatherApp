6266be8a66a6fe02505433fc068f75803d42d3b4
Maxwell AHOUNDA <127607211+Oni-90@users.noreply.github.com>
2025-02-12 09:15:55 +0000

    Update next-seven-days-weather.component.ts

--- src/app/next-seven-days-weather/next-seven-days-weather.component.ts
+++ src/app/next-seven-days-weather/next-seven-days-weather.component.ts
@@ -35,7 +35,7 @@ export class NextSevenDaysWeatherComponent {
   getCurrentDate():string{
     const today = new Date();
       const day = today.getDate();
-        const month = today.getMonth() + 1; // Months are zero-based
+        const month = today.getMonth(); // Months are zero-based
         const year = today.getFullYear();
 
         return `${day}/${month}/${year}`;
